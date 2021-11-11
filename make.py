0de785fc75291af0e1d771adc39438d92929ef79
mirmik <mirmikns@yandex.ru>
2021-11-11 18:43:43 +0300

    'a'

--- make.py
+++ make.py
@@ -5,21 +5,18 @@ import licant
 licant.include("igris")
 licant.execute_recursive("zillot", ".g.py")
 
-licant.module("zillot.drivers.avr",
-	sources = ["zillot/serial/avr_usart.c"]
-)
-
 licant.cxx_application("avr_test_build", 
 	toolchain = licant.gcc_toolchain("avr-"),
 
 	sources=["zillot/main_stub.c"],
 	
 	mdepends = [
-		"zillot.chip.avr-atmega2560",
-		"zillot.avr-common",
+		"zillot.chip.avr.atmega2560",
 		"zillot.drivers.avr",
 		"igris.include",
 		"igris.util",
+		"igris.semaphore",
+		("igris.syslock", "irqs"),
 		("igris.dprint", "stub"),
 	],
 	
@@ -27,6 +24,10 @@ licant.cxx_application("avr_test_build",
 		"arch/avr",
 		"."
 	],
+
+	cxx_flags = "-flto -ffunction-sections -fdata-sections -Wl,--gc-sections -Os -Werror=all -Werror=extra -fno-rtti -fno-exceptions",
+	cc_flags = "-flto -ffunction-sections -fdata-sections -Wl,--gc-sections -Os -Werror=all -Werror=extra -fno-exceptions",
+	ld_flags = "-flto -ffunction-sections -fdata-sections -Wl,--gc-sections -Os -Wall -Wextra -fno-rtti -fno-exceptions",
 
 	defines = ["F_CPU=16000000"]
 )
