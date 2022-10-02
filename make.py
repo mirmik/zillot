0e17e6b1e6169687006d8886f6dcb2a26305ce06
mirmik <mirmikns@yandex.ru>
2022-10-02 21:42:32 +0300

    'add stm32 script'

--- make.py
+++ make.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 import licant
+import os
 
 licant.execute("./tests/arduino2560-test/make.py")
 licant.execute("./tests/stm32-test/make.py")
 licant.execute("./tests/common/make.py")
 
 licant.fileset("all", ["runtests", "arduino2560-firmware.elf", "stm32-firmware.elf"])
+
+
+@licant.routine(deps=["all"])
+def stm32_install_test():
+    os.system("arm-none-eabi-objcopy -O binary stm32-firmware.elf stm32-firmware.bin")
+    os.system("st-flash write stm32-firmware.bin 0x8000000")
+
 
 licant.ex("all")
