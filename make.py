be25c42502de752e1e62ef4f5d5ed60cdc7a7923
mirmik <mirmikns@yandex.ru>
2022-10-24 12:55:29 +0300

    'add install stub'

--- make.py
+++ make.py
@@ -6,13 +6,20 @@ licant.execute("./tests/arduino2560-test/make.py")
 licant.execute("./tests/stm32-test/make.py")
 licant.execute("./tests/common/make.py")
 
-licant.fileset("all", ["runtests", "arduino2560-firmware.elf", "stm32-firmware.elf"])
+licant.fileset(
+    "all", ["runtests", "arduino2560-firmware.elf", "stm32-firmware.elf"])
 
 
 @licant.routine(deps=["all"])
 def stm32_install_test():
-    os.system("arm-none-eabi-objcopy -O binary stm32-firmware.elf stm32-firmware.bin")
+    os.system(
+        "arm-none-eabi-objcopy -O binary stm32-firmware.elf stm32-firmware.bin")
     os.system("st-flash write stm32-firmware.bin 0x8000000")
+
+
+@licant.routine(deps=["all"])
+def install():
+    return
 
 
 licant.ex("all")
