610ecb7cb81c4b0b2db2ebadc98d0cf4ad8b78ee
mirmik <mirmikns@yandex.ru>
2023-03-01 12:07:18 +0300

    'fix ftoa signature'

--- zillot/common/chardev.cpp
+++ zillot/common/chardev.cpp
@@ -1,14 +1,14 @@
-#include <zillot/common/chardev.h>
 #include <string.h>
+#include <zillot/common/chardev.h>
 
-int zillot::chardev::print(int32_t arg) 
-{ 
-	char buf[32];
-	char* eptr = i32toa(arg, buf, 10);
-	return write(buf, eptr - buf); 
+int zillot::chardev::print(int32_t arg)
+{
+    char buf[32];
+    char *eptr = igris_i32toa(arg, buf, 10);
+    return write(buf, eptr - buf);
 }
 
-int zillot::chardev::print(const char * arg) 
+int zillot::chardev::print(const char *arg)
 {
-	return write(arg, strlen(arg));
+    return write(arg, strlen(arg));
 }
