edd4546422e4b0f1caa11a179e8ef94364e88caa
mirmik <mirmikns@yandex.ru>
2023-03-21 15:58:48 +0300

    'some uartring details updated'

--- zillot/common/uartring.h
+++ zillot/common/uartring.h
@@ -1,62 +1,71 @@
 #ifndef ZILLOT_SERIAL_UARTRING_H
 #define ZILLOT_SERIAL_UARTRING_H
 
-#include <zillot/common/chardev.h>
-#include <zillot/common/uart.h>
 #include <igris/buffer.h>
 #include <igris/datastruct/dlist.h>
 #include <igris/datastruct/ring.h>
+#include <zillot/common/chardev.h>
+#include <zillot/common/uart.h>
 
 namespace zillot
 {
-	class uartring : public zillot::chardev
-	{
-		zillot::uart * udev = nullptr;
-		char* rxbuffer = nullptr;
-		char* txbuffer = nullptr;
-		struct ring_head rxring;
-		struct ring_head txring;
-		int refs = 0;
+    class uartring : public zillot::chardev
+    {
+        zillot::uart *udev = nullptr;
+        char *rxbuffer = nullptr;
+        char *txbuffer = nullptr;
+        struct ring_head rxring;
+        struct ring_head txring;
+        int refs = 0;
 
-	public:
-		uartring(
-			zillot::uart * udev,
-		    igris::buffer rxbuf, 
-		    igris::buffer txbuf
-		)
-			:
-			zillot::chardev(),
-			udev(udev), 
-			rxbuffer(rxbuf.data()), 
-			txbuffer(txbuf.data()),
-			rxring(RING_HEAD_INIT((unsigned int)rxbuf.size())), 
-			txring(RING_HEAD_INIT((unsigned int)txbuf.size()))
-		{}
+    public:
+        uartring(zillot::uart *udev, igris::buffer rxbuf, igris::buffer txbuf)
+            : zillot::chardev(), udev(udev), rxbuffer(rxbuf.data()),
+              txbuffer(txbuf.data()),
+              rxring(RING_HEAD_INIT((unsigned int)rxbuf.size())),
+              txring(RING_HEAD_INIT((unsigned int)txbuf.size()))
+        {
+        }
 
-		uartring(const uartring&) = delete;
-		uartring& operator=(const uartring&) = delete;
+        uartring(const uartring &) = delete;
+        uartring &operator=(const uartring &) = delete;
 
-		int read(void* data, size_t size) override;
-		int write(const void* data, size_t size) override;
-		int release() override;
-		int open(void * controller_id) override;
+        int read(void *data, size_t size) override;
+        int write(const void *data, size_t size) override;
+        int release() override;
+        int open(void *controller_id) override;
 
-		size_t room() override;
-		size_t avail() override;
+        size_t room() override;
+        size_t avail() override;
 
-		void begin(zillot::uart * udev);
-		void begin() { begin(udev); };
-		void clear();
+        void begin(zillot::uart *udev);
+        void begin()
+        {
+            begin(udev);
+        };
+        void clear();
 
-		static void uartring_irq_handler(void* priv, int code);
-	};
+        char getc()
+        {
+            char c;
+            read(&c, 1);
+            return c;
+        }
+
+        void putc(char c)
+        {
+            write(&c, 1);
+        }
+
+        static void uartring_irq_handler(void *priv, int code);
+    };
 }
 
-#define ZILLOT_UARTRING(name, uart, tsize, rsize) \
-char CONCAT2(txbuf, __LINE__)[tsize];                     \
-char CONCAT2(rxbuf, __LINE__)[rsize];                     \
-zillot::uartring name(&(uart),                             \
-	igris::buffer{CONCAT2(txbuf, __LINE__), tsize},       \
-	igris::buffer{CONCAT2(rxbuf, __LINE__), rsize});
+#define ZILLOT_UARTRING(name, uart, tsize, rsize)                              \
+    char CONCAT2(txbuf, __LINE__)[tsize];                                      \
+    char CONCAT2(rxbuf, __LINE__)[rsize];                                      \
+    zillot::uartring name(&(uart),                                             \
+                          igris::buffer{CONCAT2(txbuf, __LINE__), tsize},      \
+                          igris::buffer{CONCAT2(rxbuf, __LINE__), rsize});
 
 #endif
